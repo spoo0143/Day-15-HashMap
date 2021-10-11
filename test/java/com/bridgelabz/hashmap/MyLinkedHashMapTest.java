f32ea74221e4c8a27259c12af4a51121e69f012b
spoo0143 <gowdaspoorthi28@gmail.com>
2021-10-11 12:54:15 +0530

    ADD:Checking frequency of words in large paragraph

--- test/java/com/bridgelabz/hashmap/MyLinkedHashMapTest.java
+++ test/java/com/bridgelabz/hashmap/MyLinkedHashMapTest.java
@@ -8,7 +8,7 @@ public class MyLinkedHashMapTest {
 	
 	public void givenASentenceWhenWordsAreAddedToListShouldReturnWordFrequency() {
 		
-		String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into  paranoid situations";
+		String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into  paranoid avoidable situations";
 		
 		MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
 		String[] words = sentence.toLowerCase().split(" ");
